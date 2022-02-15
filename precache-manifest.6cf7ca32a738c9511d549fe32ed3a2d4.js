26fd569ce8170e1fce7920f8973c1c11e1807367
Chao Z <maxinimize@gmail.com>
2022-02-15 17:44:26 +0800

    Updates

---
+++ precache-manifest.6cf7ca32a738c9511d549fe32ed3a2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ae47a1877c5c56f69d5",
+    "url": "/ArtifactCardsViewer/static/css/main.8a7f1425.chunk.css"
+  },
+  {
+    "revision": "7ae47a1877c5c56f69d5",
+    "url": "/ArtifactCardsViewer/static/js/main.7ae47a18.chunk.js"
+  },
+  {
+    "revision": "ad9055319dfacb4b7f09",
+    "url": "/ArtifactCardsViewer/static/js/runtime~main.ad905531.js"
+  },
+  {
+    "revision": "4fea1bd5dfed1ecf663e",
+    "url": "/ArtifactCardsViewer/static/css/2.f7e8e74a.chunk.css"
+  },
+  {
+    "revision": "4fea1bd5dfed1ecf663e",
+    "url": "/ArtifactCardsViewer/static/js/2.4fea1bd5.chunk.js"
+  },
+  {
+    "revision": "1df1977c6c8251d275a024b808b1b1c1",
+    "url": "/ArtifactCardsViewer/index.html"
+  }
+];
